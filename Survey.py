6661c839d852244eb8e4f83c3525ce407e21f18a
Richard Chong <142849499+RichardCh971@users.noreply.github.com>
2024-09-28 18:21:48 -0400

    Add files via upload
    update the questions and each question has its own page but it has a bug where first question you have to double click next question, and for the second question it would hit next before finishing moving the slider.

--- Survey.py
+++ Survey.py
@@ -1,10 +1,36 @@
 import streamlit as st
 
+total_score = 0
+
 st.title('Survey')
 
-st.subheader("Question 1")
-x = st.slider("How do you feel when meeting new people? (1 = Very Uncomfortable, 5 = Very Comfortable)", value=3, min_value=1, max_value=5)
-result = st.button('Submit', x)
+if 'question_number' not in st.session_state:
+    st.session_state.question_number = 1
 
-if result:
-    st.success('You submitted the task!')
+def show_question_1():
+    st.subheader("Question 1")
+    st.slider("How do you feel when meeting new people? (1 = Very Uncomfortable, 5 = Very Comfortable)", value=3, min_value=1, max_value=5 )
+    if st.button("Next"):
+        st.session_state.question_number = 2
+
+
+
+def show_question_2():
+    st.subheader("Question 2")
+    st.slider("How do you feel about speaking in front of others or giving a presentation? (1 = Very Uncomfortable, 5 = Very Comfortable)", value=3, min_value=1, max_value=5)
+    if st.button("Next"):
+        st.session_state.question_number = 3
+
+
+def show_question_3():
+    st.subheader("Question 3")
+    st.write("Question 3: What is your favorite food?")
+
+
+if st.session_state.question_number == 1:
+    show_question_1()
+elif st.session_state.question_number == 2:
+    show_question_2()
+elif st.session_state.question_number == 3:
+    show_question_3()
+
