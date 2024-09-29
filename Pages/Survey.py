2e83f4ee3ccde32c157537f2533989738966577a
williamguerrero <wizwang720@gmail.com>
2024-09-29 06:54:23 -0400

    updates

--- Pages/Survey.py
+++ Pages/Survey.py
@@ -6,18 +6,35 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyCyXkpduy3uFH_gQdJBMJO4pR2ZV3dVDQw")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
+
+
 # Questions and their corresponding slider labels
 questions = {
-    "How do you feel when meeting new people?": ["Very uncomfortable", "Uncomfortable", "Neutral", "Comfortable", "Very comfortable"],
-    "How do you feel about speaking in front of others or giving a presentation?": ["Very uncomfortable", "Uncomfortable", "Neutral", "Comfortable", "Very comfortable"],
-    "Do you ever experience physical symptoms (e.g., sweating, shaking, or a racing heart) in social situations?": ["Always", "Often", "Sometimes", "Rarely", "Never"],
-    "How do you feel when attending social events like parties or networking events?": ["Very uncomfortable", "Uncomfortable", "Neutral", "Comfortable", "Very comfortable"],
-    "I find it easy to read social cues, like body language and tone of voice.": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"],
-    "I find it easy to share personal stories or experiences in a group.": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"],
-    "I feel comfortable asking strangers for help (e.g., directions, advice).": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"],
-    "I feel comfortable in social situations where I might not know many people.": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"],
-    "I feel confident initiating conversations in work or professional settings.": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"],
-    "I find it easy to make and maintain eye contact during conversations.": ["Strongly disagree", "Disagree", "Neutral", "Agree", "Strongly agree"]
+    "How do you feel when meeting new people?": ["Very uncomfortable", "Uncomfortable", "Neutral", "Comfortable",
+                                                 "Very comfortable"],
+    "How do you feel about speaking in front of others or giving a presentation?": ["Very uncomfortable",
+                                                                                    "Uncomfortable", "Neutral",
+                                                                                    "Comfortable", "Very comfortable"],
+    "Do you ever experience physical symptoms (e.g., sweating, shaking, or a racing heart) in social situations?": [
+        "Always", "Often", "Sometimes", "Rarely", "Never"],
+    "How do you feel when attending social events like parties or networking events?": ["Very uncomfortable",
+                                                                                        "Uncomfortable", "Neutral",
+                                                                                        "Comfortable",
+                                                                                        "Very comfortable"],
+    "I find it easy to read social cues, like body language and tone of voice.": ["Strongly disagree", "Disagree",
+                                                                                  "Neutral", "Agree", "Strongly agree"],
+    "I find it easy to share personal stories or experiences in a group.": ["Strongly disagree", "Disagree", "Neutral",
+                                                                            "Agree", "Strongly agree"],
+    "I feel comfortable asking strangers for help (e.g., directions, advice).": ["Strongly disagree", "Disagree",
+                                                                                 "Neutral", "Agree", "Strongly agree"],
+    "I feel comfortable in social situations where I might not know many people.": ["Strongly disagree", "Disagree",
+                                                                                    "Neutral", "Agree",
+                                                                                    "Strongly agree"],
+    "I feel confident initiating conversations in work or professional settings.": ["Strongly disagree", "Disagree",
+                                                                                    "Neutral", "Agree",
+                                                                                    "Strongly agree"],
+    "I find it easy to make and maintain eye contact during conversations.": ["Strongly disagree", "Disagree",
+                                                                              "Neutral", "Agree", "Strongly agree"]
 }
 
 # In-memory storage for user data using session state
@@ -25,46 +42,72 @@ if "score" not in st.session_state:
     st.session_state.score = 0
 if "completed_challenges" not in st.session_state:
     st.session_state.completed_challenges = []
+    #adding this to see if challenges will be saved
+
+
+
+# check this
 if "current_challenge" not in st.session_state:
     st.session_state.current_challenge = None
 if "current_question_index" not in st.session_state:
     st.session_state.current_question_index = 0
+if "challenges" not in st.session_state:
+    st.session_state.challenges = []
+if "num_challenges" not in st.session_state:
+    st.session_state.num_challenges = 0
 
 # Function to get a response from the Gemini API using generative AI
-def get_gemini_response(score):
-    try:
-        prompt = ""
-        if 10 <= score <= 17:
-            prompt = "Give me a random 1-2 sentence social challenge for someone with extreme social anxiety."
-        elif 18 <= score <= 25:
-            prompt = "Give me a random 1-2 sentence social challenge for someone with severe social anxiety."
-        elif 26 <= score <= 33:
-            prompt = "Give me a random 1-2 sentence social challenge for someone with moderate social anxiety."
-        elif 34 <= score <= 41:
-            prompt = "Give me a random 1-2 sentence social challenge for someone with mild social anxiety."
-        else:
-            prompt = "Give me a random 1-2 sentence social challenge for someone with no social anxiety."
+# def get_gemini_response(score):
+#     global prompt
+#     try:
+#
+#         if 10 <= score <= 17:
+#             prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'nonexistent level of social skills. separate each challenge with a "#". Also do not number '
+#                       'the challenges.')
+#
+#         elif 18 <= score <= 25:
+#             prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'low level of social skills. separate each challenge with a "#". Also do not number '
+#                       'the challenges.')
+#
+#         elif 26 <= score <= 33:
+#             prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'medium level of social skills. separate each challenge with a "#". Also do not number '
+#                       'the challenges.')
+#
+#         elif 34 <= score <= 41:
+#             prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'high level of social skills. separate each challenge with a "#". Also do not number '
+#                       'the challenges.')
+#
+#         elif 42 <= score <= 50:
+#             prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'extremely high/proficient level of social skills. separate each challenge with a "#". Also do '
+#                       'not number the challenges.')
+#
+#
+#
+#         # Fetch response from the API using the correct method (generate or chat)
+#
+#         response = model.generate_content(prompt)
+#
+#         challenges = response.text.split('#')
+#         challenges.pop(0)
+#
+#         st.write(challenges)
+#
+#         # response = genai.generate(
+#         #     model="gemini-1.5-flash",
+#         #     prompt=prompt,
+#         #     system_instructions=system_instructions
+#         # )
+#
+#         return challenges
+#     except Exception as e:
+#         st.error(f"Error fetching challenge: {e}")
+#         return "Could not retrieve a challenge. Please try again later."
 
-        system_instructions = """You are an AI assistant helping people overcome social anxiety through exposure therapy. 
-        The user will provide their score on a social skills assessment ranging from 10 (low) to 50 (high).
-        Based on their score, provide a suitable social challenge that is safe, clear, and promotes skill-building. 
-        Tailor the challenge to the user's skill level. 
-        Ensure the challenge is achievable but slightly pushes their boundaries to encourage growth."""
-
-        # Fetch response from the API using the correct method (generate or chat)
-
-        response = model.generate_content(prompt)
-
-        # response = genai.generate(
-        #     model="gemini-1.5-flash",
-        #     prompt=prompt,
-        #     system_instructions=system_instructions
-        # )
-
-        return response.text
-    except Exception as e:
-        st.error(f"Error fetching challenge: {e}")
-        return "Could not retrieve a challenge. Please try again later."
 
 st.title("Your Social Challenge Journey")
 # Streamlit app layout
@@ -91,7 +134,8 @@ if st.session_state.current_question_index < len(questions):
             st.session_state.score -= options.index(selected_option) + 1  # Adjust score when moving back
     with col2:
         if st.button("Next"):
-            score = options.index(selected_option) + 1
+            #check this
+            score = options.index(selected_option)
             st.session_state.score += score
             st.session_state.current_question_index += 1
 
@@ -100,20 +144,82 @@ elif st.session_state.current_question_index >= len(questions):
     score = st.session_state.score
     st.write(f"Your social skills score: {score}")
 
+    prompt = ""
+    if 10 <= score <= 17:
+        prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+                  'nonexistent level of social skills. separate each challenge with a "#". Also do not number '
+                  'the challenges.')
+
+    elif 18 <= score <= 25:
+        prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+                  'low level of social skills. separate each challenge with a "#". Also do not number '
+                  'the challenges.')
+
+    elif 26 <= score <= 33:
+        prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+                  'medium level of social skills. separate each challenge with a "#". Also do not number '
+                  'the challenges.')
+
+    elif 34 <= score <= 41:
+        prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+                  'high level of social skills. separate each challenge with a "#". Also do not number '
+                  'the challenges.')
+
+    elif 42 <= score <= 50:
+        prompt = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+                  'extremely high/proficient level of social skills. separate each challenge with a "#". Also '
+                  'do not number the challenges.')
+
+    if not st.session_state.challenges:
+        response = model.generate_content(prompt)
+        st.session_state.challenges = [challenge.strip() for challenge in response.text.split('#') if challenge.strip()]
+
+    st.write(st.session_state.challenges)
+
+
+    # challenges = response.text.split('#')
+
+
+
+
     # Get or generate a new challenge
-    if st.session_state.current_challenge is None:
-        with st.spinner('Fetching your challenge...'):
-            st.session_state.current_challenge = get_gemini_response(score)
 
     st.subheader("Today's Challenge:")
+    if st.session_state.current_challenge is None:
+        st.session_state.current_challenge = st.session_state.challenges[0]
+        st.session_state.current_challenge = st.session_state.challenges[st.session_state.num_challenges]
+
     st.write(st.session_state.current_challenge)
 
-    if st.button("Mark as Complete"):
-        st.session_state.completed_challenges.append(st.session_state.current_challenge)
-        st.session_state.current_challenge = None
 
+
+    #check if thee are more challenges left
+    if st.session_state.num_challenges <= 14:
+
+
+        if st.button("Mark as Complete"):
+            # append current challenge to the completed challenges
+
+
+            # move to the next challenge
+
+            #check if more challenges are available, if not, set a message
+            # if st.session_state.num_challenges < len(st.session_state.challenges):
+            st.session_state.num_challenges += 1
+            st.session_state.completed_challenges.append(st.session_state.current_challenge)
+            st.session_state.current_challenge = st.session_state.challenges[st.session_state.num_challenges]
+            # st.session_state.current_challenge = st.session_state.challenges[st.session_state.num_challenges]
+                # st.session_state.current_challenge = st.session_state.challenges[st.session_state.num_challenges]
+        # else:
+        #     st.session_state.current_challenge = "You've completed all the challenges!"
+
+
+# #double check this
     st.subheader("Completed Challenges:")
     st.write(st.session_state.completed_challenges)
+
+
+
 
 # Option to restart the quiz
 if st.button("Restart"):
@@ -121,3 +227,5 @@ if st.button("Restart"):
     st.session_state.completed_challenges = []
     st.session_state.current_challenge = None
     st.session_state.current_question_index = 0
+
+#
--- main.py
+++ main.py
@@ -13,12 +13,12 @@ import os
 
 # api_key = "AIzaSyCyXkpduy3uFH_gQdJBMJO4pR2ZV3dVDQw"
 
-genai.configure(api_key="AIzaSyCyXkpduy3uFH_gQdJBMJO4pR2ZV3dVDQw")
-
-st.title("Being Human")
-st.subheader("Conquer social anxiety at any level")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+# genai.configure(api_key="AIzaSyCyXkpduy3uFH_gQdJBMJO4pR2ZV3dVDQw")
+#
+# st.title("Being Human")
+# st.subheader("Conquer social anxiety at any level")
+#
+# model = genai.GenerativeModel("gemini-1.5-flash")
 
 # response = model.generate_content("How to beat my social anxiety? in 5 paragraphs")
 # st.write(response.text)
@@ -26,29 +26,29 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 # write statement below in terminal to run web app
 # streamlit run main.py
 
-
-prompt_extreme_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
-                          'nonexistent level of social skills. separate each challenge with a "#". Also do not number '
-                          'the challenges.')
-
-prompt_severe_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
-                         'low level of social skills. separate each challenge with a "#". Also do not number '
-                         'the challenges.')
-
-prompt_mod_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
-                      'medium level of social skills. separate each challenge with a "#". Also do not number '
-                      'the challenges.')
-
-prompt_mild_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
-                       'high level of social skills. separate each challenge with a "#". Also do not number '
-                       'the challenges.')
-
-prompt_no_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
-                     'extremely high/proficient level of social skills. separate each challenge with a "#". Also do '
-                     'not number the challenges.')
-
-list1 = [prompt_extreme_anxiety, prompt_severe_anxiety, prompt_mod_anxiety, prompt_mild_anxiety, prompt_no_anxiety]
-
-for i in range(5):
-    response = model.generate_content(list1[i])
-    print(response.text)
+#
+# prompt_extreme_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                           'nonexistent level of social skills. separate each challenge with a "#". Also do not number '
+#                           'the challenges.')
+#
+# prompt_severe_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                          'low level of social skills. separate each challenge with a "#". Also do not number '
+#                          'the challenges.')
+#
+# prompt_mod_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                       'medium level of social skills. separate each challenge with a "#". Also do not number '
+#                       'the challenges.')
+#
+# prompt_mild_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                        'high level of social skills. separate each challenge with a "#". Also do not number '
+#                        'the challenges.')
+#
+# prompt_no_anxiety = ('Give me 15 different but specific 1-2 sentence social challenges for someone with a '
+#                      'extremely high/proficient level of social skills. separate each challenge with a "#". Also do '
+#                      'not number the challenges.')
+#
+# list1 = [prompt_extreme_anxiety, prompt_severe_anxiety, prompt_mod_anxiety, prompt_mild_anxiety, prompt_no_anxiety]
+#
+# for i in range(5):
+#     response = model.generate_content(list1[i])
+#     print(response.text)
